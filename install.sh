739a8c5104c568a7297014c1899eb07267d1b336
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2023-02-03 06:41:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,12 @@
 echo "SCRIPT AUTO INSTALL WINDOWS by SENO6262"
 echo
 echo "Pilih OS yang ingin anda install"
-echo "[1] Windows 2019(Default)"
-echo "[2] Windows 2016"
-echo "[3] Windows 2012"
-echo "[4) Windows 10"
-echo "[5] Chat Ryan Untuk Add OS lain"
+echo "[1] Windows 2012(Default)"
 
 read -p "Pilih [1]: " PILIH OS
 
 case "$PILIHOS" in
-	1) PILIHOS="https://nixpoin.sgp1.cdn.digitaloceanspaces.com/windows2019DO.gz";;
-	2) PILIHOS="https://nixpoin.sgp1.cdn.digitaloceanspaces.com/windows2016.gz";;
-	3|"") PILIHOS="https://nixpoin.sgp1.cdn.digitaloceanspaces.com/windows2012v2.gz";;
-	4) PILIHOS="https://nixpoin.sgp1.cdn.digitaloceanspaces.com/win10.gz";;
-	5) read -p "[?] Masukkan Link GZ mu : " PILIHOS;;
+	1|"") PILIHOS="https://nixpoin.sgp1.cdn.digitaloceanspaces.com/windows2012v2.gz";;
 	*) echo "[!] Pilihan salah"; exit;;
 esac
 
